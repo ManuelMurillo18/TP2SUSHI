505dadb9dd04e14a10f54d17acca743d7b9791cb
Manuel-David <manuel.d.murillo.2003@gmail.com>
2024-11-11 13:21:03 -0500

    controller

--- controllers/index.php
+++ controllers/index.php
@@ -3,19 +3,6 @@ require 'src/database.php';
 require 'src/configuration.php';
 require 'views/index.php';
 
-if($_SERVER['REQUEST_METHOD'] == 'GET')
-{
-    $filtre = $_GET['id'];
-    $task;
-    foreach($sushi as $element)
-    {
-        if($element['idCategorie'] == $filtre)
-        {
-            $task.array_merge($element);
-        }
-    }
-
-}
 
 $pdo = databaseGetPDO(CONFIGURATIONS['database'],DB_PARAMS);
 $sushi = getAllSushi($pdo);
