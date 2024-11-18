8bee6ea5467efe14d2d6dc7296d3b091347a6632
Rubbix69 <matisgrenier@gmail.com>
2024-11-17 21:33:12 -0500

    modif

--- views/panier-achat.php
+++ views/panier-achat.php
@@ -10,12 +10,11 @@ require 'partials/header.php';
             aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarsMain">
-            <ul class="container nav navbar-nav justify-content-end">
+            <ul class="nav navbar-nav justify-content-end">
                 <li class="nav-item">
-                    <a class="nav-link nav-menu" href="/" title="Retour au menu"><span class="container mr-1"><i
-                                class="fa-solid fa-left-long fa-lg"></i></span>Retour au menu</a>
+                    <a class="nav-link nav-menu" href="/" title="Retour au menu"><i
+                            class="fa-solid fa-left-long fa-lg mr-1"></i>Retour au menu</a>
                 </li>
             </ul>
         </div>
@@ -23,85 +22,63 @@ require 'partials/header.php';
 </nav>
 
 <div class="container-fluid row align-items-start">
-    <div class="col-8"> <!-- Liste des items du panier -->
-        <div class="container row admin">
+    <div class="col-8"> <!-- Panier items -->
+        <div class="row admin">
             <div class="row align-items-end">
                 <div class="col-11">
-                    <h1><strong>Panier d'achats </strong></h1>
+                    <h1><strong>Panier d'achats</strong></h1>
                 </div>
                 <div class="col-1">
                     <h6>Prix unitaire</h6>
                 </div>
             </div>
             <hr>
-            <div class="container">
-                <div class="row">
-                    <div class="col-11">
-                        <div class="container row">
-                            <div class="col-4 cart-detail-image">
-                                <img src="/public/uploads/uramaki_thon.jpg" alt="Image de l'article"
-                                    class="cart-detail-image">
-                            </div>
-                            <div class="col-7 mt-4" cart-detail-text>
-                                <h4>Urumaki au thon</h4>
-                                <p>Thon et crème sure entourés d'une feuille d'algue et de riz vinaigré ainsi que de
-                                    graines de sésame grillées.</p>
-
-                                <div class="d-inline-flex align-items-center">
-                                    <form class="item-quantity-selected">
-                                        <input type="hidden" class="item-id" name="idItem" value="1">
-                                        <input type="hidden" class="item-unite-price" value="8.9">
-                                        <div class="d-inline-flex align-items-center">
-                                            <label class="container mr-1" for="quantities">Quantité: </label>
-                                            <select class="form-select quantities">;
-                                                <option selected value='1'>1</option>
-                                                <option value='2'>2</option>
-                                                <option value='3'>3</option>
-                                                <option value='4'>4</option>
-                                                <option value='5'>5</option>
-                                                <option value='6'>6</option>
-                                                <option value='7'>7</option>
-                                                <option value='8'>8</option>
-                                                <option value='9'>9</option>
-                                                <option value='10'>10</option>
-                                            </select>
-                                            <input class="btn btn-outline-secondary" type="submit" value="MAJ" />
-                                        </div>
-                                    </form>
-                                    <form class="item-quantity-selected">
-                                        <input type="hidden" class="item-id" name="idItem" value="1">
-                                        <input type="hidden" class="item-unite-price" value="8.9">
-                                        <div class="d-inline-flex align-items-center">
-                                            <input class="btn btn-outline-secondary" type="submit" value="Supprimer" />
-                                        </div>
-                                    </form>
-                                </div>
+            <div class="row">
+                <div class="col-11">
+                    <div class="row">
+                        <div class="col-4"><img src="/public/uploads/uramaki_thon.jpg" alt="Image de l'article"
+                                class="cart-detail-image"></div>
+                        <div class="col-7 mt-4">
+                            <h4>Urumaki au thon</h4>
+                            <p>Thon et crème sure entourés d'une feuille d'algue et de riz vinaigré ainsi que de graines
+                                de sésame grillées.</p>
+                            <div class="d-inline-flex align-items-center">
+                                <form class="item-quantity-selected" method="POST">
+                                    <input type="hidden" name="idItem" value="1">
+                                    <input type="hidden" value="8.9">
+                                    <div class="d-inline-flex align-items-center">
+                                        <label class="mr-1" for="quantities">Quantité:</label>
+                                        <select class="form-select quantities">
+                                            <?php for ($i = 1; $i <= 10; $i++)
+                                                echo "<option value='$i'" . ($i === 1 ? " selected" : "") . ">$i</option>"; ?>
+                                        </select>
+                                        <input class="btn btn-outline-secondary" type="submit" name="MAJ" value="MAJ">
+                                </form>
+                                <form class="item-quantity-selected" method="POST">
+                                    <input type="hidden" name="idItem" value="<?= $id ?>">
+                                    <input type="hidden" value="8.9">
+                                    <input class="btn btn-outline-secondary" type="submit" name="Supprimer" value="Supprimer">
+                                </form>
                             </div>
                         </div>
                     </div>
-                    <div class="col-1 mt-4">
-                        <span id="item-unite-price-formatted-{$id}">8.90 $</span>
-                    </div>
                 </div>
-                <hr>
             </div>
+            <div class="col-1 mt-4"><span>8.90 $</span></div>
         </div>
-    </div>
-    <div class="col-4"> <!-- Passer à la caisse -->
-        <div class="row admin">
-            <h3>Résumé de la commande</h3>
-            <hr>
-            <input type="hidden" id="sub-total-amount" value="8.9">
-            <h4>Sous-total (<span id="sub-total-items-count">1</span> items): <strong><span
-                        id="sub-total-amount-formatted">8.90 $</span></strong></h4>
-            <a href="/checkout" id="cart-proceed-to-checkout" class="btn cart-proceed-to-checkout">Passer à la
-                caisse</a>
-        </div>
+        <hr>
     </div>
 </div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="/public/js/cart/modified-quantities.js"></script>
+<div class="col-4"> <!-- Résumé commande -->
+    <div class="row admin">
+        <h3>Résumé de la commande</h3>
+        <hr>
+        <input type="hidden" id="sub-total-amount" value="8.9">
+        <h4>Sous-total (<span id="sub-total-items-count">1</span> items): <strong><span
+                    id="sub-total-amount-formatted">8.90 $</span></strong></h4>
+        <a href="/checkout" class="btn cart-proceed-to-checkout">Passer à la caisse</a>
+    </div>
+</div>
 </div>
 
 <?php require 'partials/footer.php'; ?>
